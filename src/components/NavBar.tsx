718c68bac6c3e362d840b14554a6f29933fda9d9
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-05-25 11:14:21 +0530

    warning removed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -56,12 +56,12 @@ const NavBar = () => {
           
           
           
-          {open===true?<div className='p-4 mr-6 md:hidden' onClick={e => { setOpen(false) }}>
+          {open===true?<div className='p-4 mr-6 md:hidden' onClick={() => { setOpen(false) }}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
              </div>:
-             <div className='p-4 mr-6 md:hidden' onClick={e => { setOpen(true) }}>
+              <div className='p-4 mr-6 md:hidden' onClick={()=>setOpen(true)}>
              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
              </svg> 
--- src/pages/Testimonial.tsx
+++ src/pages/Testimonial.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+ 
 const Testimonial = () => {
   return (
     <div>Testimonial</div>
